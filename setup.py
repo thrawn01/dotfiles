aa477c1388cff62fbec973706ed4081747fe4a7c
Derrick J. Wippler <thrawn01@gmail.com>
2010-08-31 17:42:25 +0000

    Now using distutils for installation of the shared module

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+from distutils.core import setup 
 from os import path
 import os
 import sys
@@ -45,18 +46,25 @@ def skipable( file, skip_regex ):
             return True
     return False
 
+
+
 if __name__ == "__main__":
     
     # Get my name
     prog_name = sys.argv[0].lstrip("./")
 
-    # Verify the user has ~/bin already
-    bin_path = os.path.join( os.path.expanduser("~"), "bin" )
+    # Default install directory to /usr/bin
+    bin_path = '/usr/bin'
+
+    # If the user supplied a dest directory, use that instead
+    if len(sys.argv) > 1:
+        print "-- Installing into %s" % sys.argv[1]
+        bin_path = sys.argv[1]
+
     if not path.exists( bin_path ):
         print ""
-        if not YesNo( "~/bin doesn't exist, Should I create it (Y/N) ? ", "Y" ):
+        if not YesNo( "'%s' doesn't exist, Should I create it (Y/N) ? " % bin_path, "Y" ):
             sys.exit(-1)
-
         os.makedirs( bin_path )
 
     # Get a listing of all the programs in the current directory
@@ -64,14 +72,22 @@ if __name__ == "__main__":
     for file in list:
 
         # Skip the setup.py program
-        if skipable(file, [ prog_name, '.swp', '^\.' ] ):
+        if skipable(file, [ 'dev-utils.py', prog_name, '.swp', '^\.' ] ):
             continue
 
         # Some file need to be linked as different names
         newName = rename(file,  {'svn.py':'svn'})
 
-        cmd = "ln -sf %s/%s %s/%s" % ( os.getcwd(), file, bin_path, newName )
+        cmd = "cp -f %s/%s %s/%s" % ( os.getcwd(), file, bin_path, newName )
         print " -- ", cmd
         os.system( cmd )
 
+    # Install the module
+    sys.argv = [ prog_name, 'install' ]
+    setup (name = 'dev_utils', 
+              description = "Shared library for a Collection of Development Tools",
+              author = "Derrick J. Wippler",
+              author_email = "thrawn01@gmail.com",
+              version = '1.0', 
+              py_modules = ['dev_utils'] )
     
