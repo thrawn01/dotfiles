21001b463958bdfadb030b6f02e83f7a9fae15c9
Derrick J. Wippler <thrawn01@gmail.com>
2012-03-20 11:14:45 -0500

    setup.py is pep8 compatable (except for the line length)

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #! /usr/bin/python
 
-from distutils.core import setup 
+from distutils.core import setup
+from subprocess import call
 from os import path
 import os
 import sys
 import re
 
-def getUserInput( msg, default=None, validate=None ):
+
+def getUserInput(msg, default=None, validate=None):
     while True:
-        input = raw_input( msg )
+        input = raw_input(msg)
 
         # Did we pass in a default
         if default:
@@ -17,39 +19,40 @@ def getUserInput( msg, default=None, validate=None ):
 
         # Did we ask to validate the input
         if validate:
-            if re.search( validate, input ):
+            if re.search(validate, input):
                 return input
             print "Invalid entry, Try again"
         else:
             return input
 
-def YesNo( msg, default=None ):
+
+def YesNo(msg, default=None):
     while True:
-        result = getUserInput( msg, default )
-        if re.match( "^(Y|y)$", result ):
+        result = getUserInput(msg, default)
+        if re.match("^(Y|y)$", result):
             return True
-        if re.match( "^(N|n)$", result ):
+        if re.match("^(N|n)$", result):
             return False
 
-def rename( file, rename_list ):
-    for old,new in rename_list.items():
+
+def rename(file, rename_list):
+    for old, new in rename_list.items():
         #print ("Rename: '%s' '%s'" % (old, new))
         if file == old:
             return new
     return file
 
 
-def skipable( file, skip_regex ):
+def skipable(file, skip_regex):
     for regex in skip_regex:
         #print ("regex: '%s' '%s'" % (regex, file))
-        if re.search( regex, file ):
+        if re.search(regex, file):
             return True
     return False
 
 
-
 if __name__ == "__main__":
-    
+
     # Get my name
     prog_name = sys.argv[0].lstrip("./")
 
@@ -58,7 +61,7 @@ if __name__ == "__main__":
     if home_dir == '':
         print "-- Could not determine your home directory"
         sys.exit(-1)
-    
+
     bin_path = os.path.join(home_dir, "bin")
 
     # If the user supplied a dest directory, use that instead
@@ -66,34 +69,35 @@ if __name__ == "__main__":
         print "-- Installing into %s" % sys.argv[1]
         bin_path = sys.argv[1]
 
-    if not path.exists( bin_path ):
-        print ""
-        if not YesNo( "'%s' doesn't exist, Should I create it (Y/N) ? " % bin_path, "Y" ):
+    if not path.exists(bin_path):
+        question = "\n-- '%s' doesn't exist, create it (Y/N) ? " % bin_path
+        if not YesNo(question, "Y"):
             sys.exit(-1)
-        os.makedirs( bin_path )
+        os.makedirs(bin_path)
 
     # Get a listing of all the programs in the current directory
-    list = os.listdir( os.getcwd() )
+    list = os.listdir(os.getcwd())
     for file in list:
 
         # Skip the setup.py program
-        if skipable(file, [ 'dev-utils.py', prog_name, '.swp', '^\.', '\.vim', 'rc$', 'README' ] ):
+        if skipable(file, ['dev-utils.py', prog_name, '.swp', '^\.',
+                '\.vim', 'rc$', 'README']):
             continue
 
         # Some file need to be linked as different names
-        newName = rename(file,  {'svn.py':'svn'})
+        newName = rename(file,  {'svn.py': 'svn'})
 
-        cmd = "cp -f %s/%s %s/%s" % ( os.getcwd(), file, bin_path, newName )
+        cmd = "cp -f %s/%s %s/%s" % (os.getcwd(), file, bin_path, newName)
         print " -- ", cmd
-        os.system( cmd )
+        call(cmd, shell=True)
 
-    os.system('mkdir -p ~/.vim/colors');
-    os.system('mkdir -p ~/.vimswap');
-    os.system('cp thrawn.vim ~/.vim/colors');
-    os.system('cp vimrc ~/.vimrc');
-    os.system('cp gvimrc ~/.gvimrc');
+    call('mkdir -p ~/.vim/colors', shell=True)
+    call('mkdir -p ~/.vimswap', shell=True)
+    call('cp thrawn.vim ~/.vim/colors', shell=True)
+    call('cp vimrc ~/.vimrc', shell=True)
+    call('cp gvimrc ~/.gvimrc', shell=True)
 
-    # Print out 
+    # Print out
     print """
 # Add this to .bashrc or .bash_profile
 export PATH="$PATH:~/bin"
@@ -116,4 +120,3 @@ C4="\[\e[1;34m\]" # <- seperator color (..[ ]..)
 PROMPT='>'
 export PS1="$C3$C4..( $C2\u$C1@$C3\h$C1 ($C2\$(current_branch)$C1): $C2\w$C1$C1 : $C2\t$C1 $C4)..\n$C3$C2$PROMPT$C1$PROMPT$C0 "
 """
-
