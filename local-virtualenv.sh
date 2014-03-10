0d01c40d697a6ea96f1ebb028914ecd59d821267
Derrick J. Wippler <thrawn01@gmail.com>
2014-03-10 15:51:45 -0500

    Improved local-virtualenv.sh works on osx now

--- local-virtualenv.sh
+++ local-virtualenv.sh
@@ -1,27 +1,33 @@
+# Clean up previous runs
 rm -rf ~/.virtualenv 2> /dev/null
-mkdir ~/.virtualenv
-cd ~/.virtualenv
+rm -rf ~/.virtualenv-temp 2> /dev/null
+mkdir ~/.virtualenv-temp
+
+# Copy the wheels so virtualenv.py can find them
+cp wheelhouse/* ~/.virtualenv-temp
+
+cd ~/.virtualenv-temp
 
 echo " -- Fetching virtualenv.py"
 curl -L -o virtualenv.py https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 
-echo " -- Fetching git-pip.py"
-curl -L -o get-pip.py https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+echo " -- Create virtualenv-temp"
+python virtualenv.py ~/.virtualenv-temp
 
-echo " -- Fetching ez_setup.py"
-curl -L -o ez_setup.py https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+echo " -- Activate the virtualenv-temp"
+source ~/.virtualenv-temp/bin/activate
+
+echo " -- Install virtualenv VIA pip"
+pip install virtualenv
 
 echo " -- Create the virtualenv"
-python virtualenv.py ~/.virtualenv
+virtualenv --no-site-packages ~/.virtualenv
 
 echo " -- Activate the virtualenv"
-source ~/.virtualenv/bin/activate
+source ~/.virtualenv-temp/bin/activate
 
-echo " -- Run ez_setup.py"
-python ez_setup.py
-
-echo " -- Run get-pip.py"
-python get-pip.py
+# Clean temp environment
+rm -rf ~/.virtualenv-temp
 
 echo " -- Now Activiate the virtualenv"
 echo "VIRTUAL_ENV_DISABLE_PROMPT=1"
