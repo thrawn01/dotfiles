6bcd0ca224a4221283c099c54847ef2275245498
Derrick J. Wippler <thrawn01@gmail.com>
2016-08-19 23:13:24 -0500

    Fixed run-etcd.sh and moved to /bin

--- bin/run-etcd.sh
+++ bin/run-etcd.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-HOST=192.168.15.10
+
+HOST=$(shell docker-machine ip default 2> /dev/null)
 if [ $(docker ps -a | grep -ci etcd) -eq 0 ]; then
     docker run -d --name etcd -p 2379-2380:2379-2380 -p 4001:4001 -p 7001:7001 -v /tmp/etcd:/tmp/etcd \
-        quay.io/coreos/etcd:v3.0.0-beta.0 --data-dir /tmp/etcd \
+        quay.io/coreos/etcd:latest --data-dir /tmp/etcd \
         -name etcd0 \
         -advertise-client-urls http://${HOST}:2379,http://${HOST}:4001 \
         -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
